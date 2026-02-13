9ab49303caeeff74576dac0cc4a0bd69f93ae513
khanz5-coder <khanz5@tcd.ie>
2026-02-13 15:11:34 +0000

    up

--- tutorials ZK/tutorial04.R
+++ tutorials ZK/tutorial04.R
@@ -64,11 +64,68 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 graduation <- read.table("http://statmath.wu.ac.at/courses/StatsWithR/Powers.txt")
 
 # (a) Perform a logistic regression of hsgrad on the other variables in the data set.
+
+#first we need to convert the data into factors that are character vectors 
+
+variables <- c("hsgrad", "nonwhite", "mhs", "fhs", "intact")
+graduation[variables] <- lapply(graduation[variables], factor)
+
+#runnign the regression 
+reg1 <- glm(
+  hsgrad ~ nonwhite + mhs + fhs + income + nsibs + asvab + intact,
+  data = graduation,
+  family = "binomial")
 # Compute a likelihood-ratio test of the omnibus null hypothesis that none of the explanatory variables influences high-school graduation. 
+grad_null <- glm(hsgrad ~ 1, data = graduation, family = binomial)
+anova(grad_null, reg1, test = "LRT")
+
+#at least one of the variables in the model explain the variation in the model. 
+
 # Then construct 95-percent confidence intervals for the coefficients of the seven explanatory variables. 
+confint(reg1)
 # What conclusions can you draw from these results? Finally, offer two brief, but concrete, interpretations of each of the estimated coefficients of income and intact.
 
-# (b) The logistic regression in the previous problem assumes that the partial relationship between the log-odds of high-school graduation and number of siblings is linear. 
+# (b) The logistic regression in the previous problem assumes that the
+#partial relationship between the log-odds of high-school graduation and number of siblings is linear.
+
+graduation$nsib_f <- factor(graduation$nsibs)
+
+m_factor <- glm(
+  hsgrad ~ nonwhite + mhs + fhs + income + nsib_f + asvab + intact,
+  data = graduation,
+  family = "binomial")
+
+anova(reg1, m_factor, test = "LRT")
+
+unique(graduation$nsib_f)
+table(graduation$nsib_f)
+
+#after running we see that -3, 15, and 17 do not have a sufficent number of cases. 
+grad_clean <- subset(graduation, nsibs >= 0)
+grad_clean$nsib_f <- cut(
+  grad_clean$nsibs,
+  breaks = c(-1, 1, 3 ,5, 10, 20),
+  labels <- c("0-1", "2-3", "4-5", "6-10", "11+")
+)
+
+unique(grad_clean$nsib_f)
+table(grad_clean$nsib_f) 
+
+m_factor2 <- glm(
+  hsgrad ~ nonwhite + mhs + fhs + income + nsib_f + asvab + intact,
+  data = grad_clean,
+  family = "binomial")
+
+m_f <- glm(
+  hsgrad ~ nonwhite + mhs + fhs + income + nsibs + asvab + intact,
+  data = grad_clean,
+  family = "binomial")
+summary(m_factor2)
+
+anova(m_f, m_factor2, test = "LRT")
+#since we have the same amountof variables, 
+#assuming linearity improves the model, factorizing did not improve the model.
+#taking it as a categorocal model did not do a good job therefore linearity is ideal. 
 # Test for nonlinearity by fitting a model that treats nsibs as a factor, performing an appropriate likelihood-ratio test. 
 # In the course of working this problem, you should discover an issue in the data. 
 # Deal with the issue in a reasonable manner. 
