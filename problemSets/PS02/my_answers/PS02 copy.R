702313fdf31c7ba01f6d3a7978d58b121484d137
khanz5-coder <khanz5@tcd.ie>
2026-02-17 16:45:01 +0000

    update ps02

--- problemSets/PS02/my_answers/PS02 copy.R
+++ problemSets/PS02/my_answers/PS02 copy.R
@@ -39,25 +39,66 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # load data
 load(url("https://github.com/ASDS-TCD/StatsII_2026/blob/main/datasets/climateSupport.RData?raw=true"))
 
-str(climateSupport)
+str(climateSupport) #unordering data set for ease of analysis
 climateSupport$countries <- factor(climateSupport$countries, ordered = FALSE)
 climateSupport$sanctions <- factor(climateSupport$sanctions, ordered = FALSE)
 
-m1 <- glm(
+m1 <- glm( #running an additive model
   choice ~ countries + sanctions,
   data = climateSupport,
   family = "binomial")
 
-summary(m1)
-
+summary(m1) #summarizing
 stargazer(m1)
 
-m_null <- glm(choice ~ 1, data = climateSupport, family = binomial)
+m_null <- glm(choice ~ 1, data = climateSupport, family = binomial) 
+#running a null model
+anova(m_null, m1, test = "LRT") 
+#running an ftest.
 
-gftest <- anova(m_null, m1, test = "LRT")
-stargazer(gftest)
+# Analysis of Deviance Table
+# 
+# Model 1: choice ~ 1
+# Model 2: choice ~ countries + sanctions
+# Resid. Df Resid. Dev Df Deviance  Pr(>Chi)    
+# 1      8499      11783                          
+# 2      8494      11568  5   215.15 < 2.2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
+#2a
+diff_s5_15 <- coef(m1)["sanctions15%"] - coef(m1)["sanctions5%"]
+# -0.3251028 
+or <- exp(diff_s5_15)
+# 0.7224531 
+orpercent <- (or - 1) * 100
+# -27.75469 
+
+#2c
+#computing for 80/192 country participation and 0 sanctions
+odds_80 <- coef(m1)["(Intercept)"] + coef(m1)["countries80 of 192"]
+#calculating the probablity 
+probability_80 <- exp(odds_80) / (1 + exp(odds_80))
+#checking the result. 
+probability_80
+# 0.5159191 
+
+odds5 <- odds5 - 1
+Q2C <- odds5
+# 0.3998442
 
 m_interaction <- glm(choice ~ countries * sanctions,
                      family = binomial, data = climateSupport)
 
+summary(m_interaction)
+stargazer(m_interaction)
+#creating a model with an interactive term
 anova(m1, m_interaction, test = "LRT")
+
+# Analysis of Deviance Table
+# Model 1: choice ~ countries + sanctions
+# Model 2: choice ~ countries * sanctions
+# Resid. Df Resid. Dev Df Deviance Pr(>Chi)
+# 1      8494      11568                     
+# 2      8488      11562  6   6.2928   0.3912
